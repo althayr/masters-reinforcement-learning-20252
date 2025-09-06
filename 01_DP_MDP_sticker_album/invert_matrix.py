3aeebafd595e33bbc8a85d2e20a03d328efeaaa8
althayr <althabrandao@gmail.com>
2025-09-06 18:02:37 -0300

    script to compute the bellman equation solution

--- 01_DP_MDP_sticker_album/invert_matrix.py
+++ 01_DP_MDP_sticker_album/invert_matrix.py
@@ -8,3 +8,27 @@ P = np.array([
     [0, 0.5, 0.5],
     [0, 0, 1],
 ])
+gamma = 0.5
+I = np.identity(3)
+
+inv = np.linalg.inv(I - gamma * P)
+
+solution = inv @ R
+solution
+
+
+import numpy as np
+
+R = np.array([1, 1, 0])
+P = np.array([
+    [0.5, 0.5, 0],
+    [0, 0.5, 0.5],
+    [0, 0, 1],
+])
+gamma = 0.5
+I = np.identity(3)
+
+inv = np.linalg.inv(I - gamma * P)
+
+solution = inv @ R
+solution
